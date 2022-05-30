0667411cfbda973b227559c602f30704d2398572
csumn <csumn13@gmail.com>
2022-05-30 23:36:39 +0530

    about to end

---
+++ JS/BottomHeading.js
@@ -0,0 +1,20 @@
+fetch('http://localhost:3000/webpagedata4')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        appendData(data)
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+
+function appendData(data) {
+    console.log(data);
+    let innerHtml;
+    innerHtml = `<p class = "bottom-heading-titleOne"> ${data.TitleLine1}</p>
+    <p class = "bottom-heading-titleTwo"> ${data.TitleLine2}</p>`
+    document.querySelector("#bottom-heading").innerHTML = innerHtml;
+}
--- JS/slideshowcontent.js
+++ JS/slideshowcontent.js
@@ -67,6 +67,136 @@ function appendData(data) {
     <p class="TitleOfCard onhover">${data[8].Title}</p>
     <p class="DescriptionOfCard onhover">${data[8].Description}</p>`
     document.querySelector("#card8").innerHTML = card9;
+
+    let card10;
+    card10 = `<img class="onhover" src="${data[9].image} "  width="350 px" height="450vh" alt=""/>
+    <p class="TitleOfCard onhover">${data[9].Title}</p>
+    <p class="DescriptionOfCard onhover">${data[9].Description}</p>`
+    document.querySelector("#card9").innerHTML = card10;
+
+    let card11;
+    card11 = `<img class="onhover" src="${data[10].image} "  width="350 px" height="450vh" alt=""/>
+    <p class="TitleOfCard onhover">${data[10].Title}</p>
+    <p class="DescriptionOfCard onhover">${data[10].Description}</p>`
+    document.querySelector("#card10").innerHTML = card11;
+
+    let card12;
+    card12 = `<img class="onhover" src="${data[11].image} "  width="350 px" height="450vh" alt=""/>
+    <p class="TitleOfCard onhover">${data[11].Title}</p>
+    <p class="DescriptionOfCard onhover">${data[11].Description}</p>`
+    document.querySelector("#card11").innerHTML = card12;
+
+    let card13;
+    card13 = `<img class="onhover" src="${data[12].image} "  width="350 px" height="450vh" alt=""/>
+    <p class="TitleOfCard onhover">${data[12].Title}</p>
+    <p class="DescriptionOfCard onhover">${data[12].Description}</p>`
+    document.querySelector("#card12").innerHTML = card13;
+
+    let card14;
+    card14 = `<img class="onhover" src="${data[13].image} "  width="350 px" height="450vh" alt=""/>
+    <p class="TitleOfCard onhover">${data[13].Title}</p>
+    <p class="DescriptionOfCard onhover">${data[13].Description}</p>`
+    document.querySelector("#card13").innerHTML = card14;
+
+    let card15;
+    card15 = `<img class="onhover" src="${data[14].image} "  width="350 px" height="450vh" alt=""/>
+    <p class="TitleOfCard onhover">${data[14].Title}</p>
+    <p class="DescriptionOfCard onhover">${data[14].Description}</p>`
+    document.querySelector("#card14").innerHTML = card15;
+
+    let card16;
+    card16 = `<img class="onhover" src="${data[15].image} "  width="350 px" height="450vh" alt=""/>
+    <p class="TitleOfCard onhover">${data[15].Title}</p>
+    <p class="DescriptionOfCard onhover">${data[15].Description}</p>`
+    document.querySelector("#card15").innerHTML = card16;
+
+    let card17;
+    card17 = `<img class="onhover" src="${data[16].image} "  width="350 px" height="450vh" alt=""/>
+    <p class="TitleOfCard onhover">${data[16].Title}</p>
+    <p class="DescriptionOfCard onhover">${data[16].Description}</p>`
+    document.querySelector("#card16").innerHTML = card17;
+
+    let card18;
+    card18 = `<img class="onhover" src="${data[17].image} "  width="350 px" height="450vh" alt=""/>
+    <p class="TitleOfCard onhover">${data[17].Title}</p>
+    <p class="DescriptionOfCard onhover">${data[17].Description}</p>`
+    document.querySelector("#card17").innerHTML = card18;
 }
 
 
+
+
+
+
+
+
+
+
+
+
+// let card10;
+// card10 = `<img class="onhover" src="${data[9].image} "  width="350 px" height="450vh" alt=""/>
+// <p class="TitleOfCard onhover">${data[9].Title}</p>
+// <p class="DescriptionOfCard onhover">${data[9].Description}</p>`
+// document.querySelector("#card8").innerHTML = card10;
+
+// let card11;
+// card11 = `<img class="onhover" src="${data[10].image} "  width="350 px" height="450vh" alt=""/>
+// <p class="TitleOfCard onhover">${data[10].Title}</p>
+// <p class="DescriptionOfCard onhover">${data[10].Description}</p>`
+// document.querySelector("#card8").innerHTML = card11;
+
+// let card12;
+// card12 = `<img class="onhover" src="${data[11].image} "  width="350 px" height="450vh" alt=""/>
+// <p class="TitleOfCard onhover">${data[11].Title}</p>
+// <p class="DescriptionOfCard onhover">${data[11].Description}</p>`
+// document.querySelector("#card8").innerHTML = card12;
+
+
+
+// {,
+//     {
+//       "Title": "Zapotal",
+//       "Description": "Guanacaste, Costa Rica",
+//       "image": "../Assets/images/Cards/card10.jpg"
+//     },
+//     {
+//       "Title": "James Island",
+//       "Description": "Columbia Canada",
+//       "image": "../Assets/images/Cards/card12.jpg"
+//     },
+//     {
+//       "Title": "Costa Terra",
+//       "Description": "Comporta, Portugal",
+//       "image": "../Assets/images/Cards/card13.jpg"
+//     },
+//     {
+//       "Title": "Driftwood",
+//       "Description": "Austin, Texas",
+//       "image": "../Assets/images/Cards/card14.jpg"
+//     },
+//     {
+//       "Title": "Troubadour",
+//       "Description": "Nashville, Tannessee",
+//       "image": "../Assets/images/Cards/card15.jpg"
+//     },
+//     {
+//       "Title": "Playa Grande",
+//       "Description": "Rio San Juan, Dominician Republic",
+//       "image": "../Assets/images/Cards/card1.jpg"
+//     },
+//     {
+//       "Title": "The Summit",
+//       "Description": "Las Vegas, Nevada",
+//       "image": "../Assets/images/Cards/card13.jpg"
+//     },
+//     {
+//       "Title": "Dune Deck",
+//       "Description": "Westhampton Beach, NY",
+//       "image": "../Assets/images/Cards/card12.jpg"
+//     },
+//     {
+//       "Title": "Silo Ridge",
+//       "Description": "Amenia, New York",
+//       "image": "../Assets/images/Cards/card.jpg"
+//     }}
